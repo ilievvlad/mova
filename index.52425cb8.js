78a3a0b3930d297ae241ff6969802a8169373c64
ilievvlad <91153606+ilievvlad@users.noreply.github.com>
2022-12-08 12:29:05 +0000

    Deploying to gh-pages from @ ilievvlad/mova@f1eee0ed1f4282224dd1d3785ff7cab980943969 🚀

---
+++ index.52425cb8.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.addEventListener("click",(function(e){const c=e.target;c.classList.contains("search-btn")?document.querySelector(".header").classList.toggle("search-active"):!c.closest(".search")&&document.querySelector(".search.search-active")&&document.querySelector(".search__form").classList.remove("search-active")}))};
+//# sourceMappingURL=index.52425cb8.js.map
---
+++ index.d2515b17.js
@@ -0,0 +1,2 @@
+window.onload=function(){document.addEventListener("click",(function(e){var c=e.target;c.classList.contains("search-btn")?document.querySelector(".header").classList.toggle("search-active"):!c.closest(".search")&&document.querySelector(".search.search-active")&&document.querySelector(".search__form").classList.remove("search-active")}))};
+//# sourceMappingURL=index.d2515b17.js.map
